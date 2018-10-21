74bd05272f1a2b513b0b1535b93486b22b3e71d3
Cyril Sevastyanov <ksevastyano@imigize.com>
2018-10-21 10:36:03 +0300

    Первые шаги на Питоне, 2й урок

---
+++ les1.py
@@ -0,0 +1,27 @@
+#коммент
+print("Доброе утро")
+print("privet")
+name = input("Ваше имя: ")
+print(name, ", добро пожаловать в мир пайтон")
+answer = input("Давайте поработаем?(Y/N)")
+action = "1 помыть полы"
+action2 = "2 помыть посуду"
+action3 = "3 подготовиться к работе"
+action4 = "4 заняться выбором дизайна дома"
+
+if answer == 'Y':
+    print(action, action2, action3, action4)
+    answer2 = input( "Какой действие ты хочешь сделать?(1/2/3/4)")
+    print("Молодец. Ты выбрала действие: ")
+    if answer2 == '1':
+        print(action)
+    elif answer2 == '2': 
+        print(action2)
+    elif answer2 == '3':
+        print(action3)
+    elif answer2 == '4':
+        print(action4)
+elif answer == 'N':
+    print("До свидания!")
+else:
+    print("Неизвестный выбор")
